3af33075ebfcd027a36e70998e2eba3b7d481829
bilalsevis <bilalresmi@gmail.com>
2019-11-08 17:25:32 +0300

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,6 +4,8 @@ import '../App.css';
 
 function Banner () {
     // referans https://cdn.dribbble.com/users/380831/screenshots/7118539/media/c8aed4a7ea3dd89e3554b5ca4fc3ef20.png
+    // ref https://dribbble.com/shots/7118539-PaperplaneCo-Creative-Agency-landing-page-design
+    // ref https://dribbble.com/shots/7160388-PaperplaneCo-Creative-Agency-landing-page-design/attachments/163344?mode=media
     // ref https://robotic.netlify.com/index-demo-1.html
     // ref http://html.xpeedstudio.com/agmycoo/index-v4.html
     // ref http://quomodosoft.com/html/wan/wan/home5-sass.html
