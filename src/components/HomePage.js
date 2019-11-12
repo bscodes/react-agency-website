36fae88cf8e56fd2e86c5d4f9d38b14d4f5ea40d
bilalsevis <bilalresmi@gmail.com>
2019-11-12 17:58:07 +0300

    added responsive styles

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './Navbar';
+import Navbar from './Navbar/Navbar';
 import Banner from './Banner';
 import '../App.scss';
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import '../App.scss';
+import './Navbar.scss';
 
 const Navbar = () => {
     return(
