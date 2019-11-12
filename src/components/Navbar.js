77850ccd65650f0b2b3d6b939da6c7ec5df04abc
bilalsevis <bilalresmi@gmail.com>
2019-11-12 17:38:45 +0300

    added responsive styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
                     <Link to="/" className="navbar-brand">A.</Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon">
-                            <i class="fi-xwluxl-three-bars-wide"></i>
+                            <i className="fi-xwluxl-three-bars-wide"></i>
                         </span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
