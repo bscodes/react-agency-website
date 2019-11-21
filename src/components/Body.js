816ef9679aba1e911a1f6636f27c2e0518f4d904
bilalsevis <bilalresmi@gmail.com>
2019-11-21 17:49:14 +0300

    update

--- src/components/Body.js
+++ src/components/Body.js
@@ -30,9 +30,9 @@ const Body = () => {
                     </div>
                 </div>
             </section>
-
-            <Portfolio/>
             
+            <Portfolio/>
+
             <section className="lead">
                 <div className="container text-center">
                     <div className="row align-items-center">
--- src/pages/Works.js
+++ src/pages/Works.js
@@ -1,6 +1,4 @@
 import React from 'react';
-// eslint-disable-next-line
-import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
 import Portfolio from '../components/Portfolio';
 import Footer from '../components/Footer';
