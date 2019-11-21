a8856f0b39020b8c9a2681876b32abd690335946
bilalsevis <bilalresmi@gmail.com>
2019-11-21 17:49:06 +0300

    created team members object

---
+++ src/data/team.js
@@ -0,0 +1,48 @@
+const team = [
+    {
+        "id": 0,
+        "image": [],
+        "name": "Desmond Eagle",
+        "position": "Co-Founder / President"
+    },{
+        "id": 1,
+        "image": [],
+        "name": "Will Barrow",
+        "position": "Client Strategist"
+    },{
+        "id": 2,
+        "image": [],
+        "name": "Jarvis Pepperspray",
+        "position": "Paid Media Specialist"
+    },{
+        "id": 3,
+        "image": [],
+        "name": "Russell Sprout",
+        "position": "Client Strategist"
+    },{
+        "id": 4,
+        "image": [],
+        "name": "Gibson Montgomery",
+        "position": "Content Specialist"
+    },{
+        "id": 5,
+        "image": [],
+        "name": "Bartholomew Shoe",
+        "position": "Associate Director of Content Strategy and Social Impact"
+    },{
+        "id": 6,
+        "image": [],
+        "name": "Eric Widget",
+        "position": "Client Strategist"
+    },{
+        "id": 7,
+        "image": [],
+        "name": "Weir Doe",
+        "position": "Co-Founder / Vice President"
+    },{
+        "id": 8,
+        "image": [],
+        "name": "Hugh Saturation",
+        "position": "Designer"
+    }
+]
