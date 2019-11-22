6c894da71914455da1a4a274c844aba43a699e0c
Bilal Sevinc <bilalresmi@gmail.com>
2019-11-23 00:25:57 +0300

    Updates

---
+++ precache-manifest.6e105b65f230f22f4083cbeaa7c3694f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a02075812b452e3d289ef0dc7cac6161",
+    "url": "/react-agency-website/index.html"
+  },
+  {
+    "revision": "82c428daaa4c4cd8dee2",
+    "url": "/react-agency-website/static/css/main.f90d9518.chunk.css"
+  },
+  {
+    "revision": "04aef39aeda9fbe0c601",
+    "url": "/react-agency-website/static/js/2.6ae790ee.chunk.js"
+  },
+  {
+    "revision": "82c428daaa4c4cd8dee2",
+    "url": "/react-agency-website/static/js/main.3bfc79e5.chunk.js"
+  },
+  {
+    "revision": "5a75c24d1420cf01d4cd",
+    "url": "/react-agency-website/static/js/runtime-main.5c7efcf9.js"
+  },
+  {
+    "revision": "5f86af978913654b7f6279eb19782687",
+    "url": "/react-agency-website/static/media/like.5f86af97.png"
+  },
+  {
+    "revision": "eb77bfb55efc0021e68a6186ce1cadc1",
+    "url": "/react-agency-website/static/media/p1.eb77bfb5.jpg"
+  },
+  {
+    "revision": "d13486a103ab741205326db427566332",
+    "url": "/react-agency-website/static/media/p2.d13486a1.jpg"
+  },
+  {
+    "revision": "2d13453bfed9b8622b5e2abd7399d7c9",
+    "url": "/react-agency-website/static/media/p3.2d13453b.jpg"
+  },
+  {
+    "revision": "b33b9181020763573debbb34c3920778",
+    "url": "/react-agency-website/static/media/p4.b33b9181.jpg"
+  },
+  {
+    "revision": "b4ea08bb3b40e600dbe457608289ccb3",
+    "url": "/react-agency-website/static/media/p5.b4ea08bb.jpg"
+  },
+  {
+    "revision": "57b8821639388c70be74a4d8a3a54ff7",
+    "url": "/react-agency-website/static/media/p6.57b88216.jpg"
+  },
+  {
+    "revision": "4164b7fa32d18a065900748edc6c4b09",
+    "url": "/react-agency-website/static/media/shape2.4164b7fa.png"
+  }
+]);
